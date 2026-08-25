d211a7c6fe8fce8a966c17097c38b54237a3ca3a
rupviroy-ui <rupviroy@gmail.com>
2026-08-25 13:22:59 +0530

    chorwcddfffsdssddfdddde

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1483,7 +1483,7 @@ export default function CheckoutPage() {
       <StickyOrderBar
         items={items}
         subtotal={subtotal}
-        mrpTotal={mrpTotal + shipping}
+        mrpTotal={subtotal + shipping}
         shipping={shipping}
         payableTotal={payableTotal}
         totalSavings={totalSavings}
